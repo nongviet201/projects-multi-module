52afcfe9fd531276571ca044cc2d3e31ca8d4bdf
ADMIN <viettk2015@gmail.com>
2024-07-25 00:24:34 +0700

    booking stage-one fix all bug

--- cinema/cinema-web-theme/src/main/java/com/nongviet201/cinema/web/theme/controller/webRender/BookingRenderRename.java
+++ cinema/cinema-web-theme/src/main/java/com/nongviet201/cinema/web/theme/controller/webRender/BookingRenderRename.java
@@ -25,6 +25,6 @@ public class BookingRenderRename {
                 "movies",
                 movieService.getAllPublishMoviesOrderByReleaseDate()
         );
-        return "booking/stage-one";
+        return "booking/stage/stage-one";
     }
 }
