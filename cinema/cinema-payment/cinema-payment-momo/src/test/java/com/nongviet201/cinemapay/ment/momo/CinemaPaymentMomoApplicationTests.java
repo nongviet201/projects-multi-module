68ef77d6bd7542d9058a5356e9294b198a8c8687
ADMIN <viettk2015@gmail.com>
2024-08-14 21:21:29 +0700

    update vnpay

---
+++ cinema/cinema-payment/cinema-payment-momo/src/test/java/com/nongviet201/cinemapay/ment/momo/CinemaPaymentMomoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.nongviet201.cinemapay.ment.momo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CinemaPaymentMomoApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
